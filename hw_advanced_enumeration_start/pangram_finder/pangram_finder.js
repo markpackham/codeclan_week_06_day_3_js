1f53b75299e36bc34edd26664622d7363a97ec91
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-05-07 08:09:12 +0100

    very limited Pangram check

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,20 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+  this.alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
+  // lower case, remove space, turn to array
+  this.phrase = phrase.toLowerCase().replace(/\s/g, "").split("");
+};
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  let uniq_letters = [...new Set(this.phrase)];
+  console.log(uniq_letters.sort().toString());
+  console.log(this.alphabet.sort().toString());
+  const pangrams = uniq_letters.sort().toString();
+  const alpha = this.alphabet.sort().toString();
+  if (pangrams === alpha) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 module.exports = PangramFinder;
