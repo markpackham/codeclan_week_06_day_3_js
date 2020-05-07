0691472658716253f1fa1c0340c12d5863d3f77a
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-05-07 08:40:15 +0100

    refactor

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -3,14 +3,12 @@ const PangramFinder = function (phrase) {
   // lower case, remove space, turn to array
   this.phrase = phrase
     .toLowerCase()
-    .replace(/[^a-zA-Z]/g, "")
+    .replace(/[^a-z]/g, "")
     .split("");
 };
 
 PangramFinder.prototype.isPangram = function () {
   let uniq_letters = [...new Set(this.phrase)];
-  console.log(uniq_letters.sort().toString());
-  console.log(this.alphabet.sort().toString());
   const pangrams = uniq_letters.sort().toString();
   const alpha = this.alphabet.sort().toString();
   if (pangrams === alpha) {
